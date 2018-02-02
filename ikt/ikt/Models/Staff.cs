b8d1150ed7281cbc1f9542395da6c59e2be6cee6
Marcus Perhamn <marcus@perhamn.se>
2018-02-02 10:37:24 +0100

    Added staff model

---
+++ ikt/ikt/Models/Staff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ikt.Models
+{
+    public class Staff
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}

4cf9ba290c5374ce942db95b046c66b1b2a02753
Christoffer Ekström <31304101+ChristofferEkstrom@users.noreply.github.com>
2018-02-05 09:07:26 +0100

    IktClass Added

---
+++ ikt/ikt/Models/IktClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ikt.Models
+{
+    public partial class IktClass
+    {
+        public int ID { get; set; }
+        public int? ClassID { get; set; }
+        public virtual Class Class { get; set; }
+        public int? IktID { get; set; }
+        public virtual Ikt Ikt { get; set; }
+    }
+}

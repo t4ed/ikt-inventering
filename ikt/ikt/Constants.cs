c6090ea6f8124f867f66fe6846a6478e6a14bc9a
Marcus Perhamn <marcus@perhamn.se>
2018-02-06 10:43:50 +0100

    Finalized login system

--- ikt/ikt/Constants.cs
+++ ikt/ikt/Constants.cs
@@ -9,57 +9,7 @@ namespace ikt
 {
     public class Constants
     {
-        private static iktContext db = new iktContext();
-
         public static int UserNameLength { get; } = 5;
-
-        public static bool CheckIfValidUser(string userEmail)
-        {
-            string userName = userEmail.Split('@')[0];
-            if (userName.Length == UserNameLength || Developers.Contains(userName))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public static Staff GetStaffInfo(string userEmail)
-        {
-            string username = userEmail.Split('@')[0];
-
-            var staffdata = from Staff in db.Staff
-                        where Staff.Username == username
-                        select Staff;
-
-            
-
-            if (staffdata.Count() == 1)
-            {
-                Staff staff = new Staff();
-
-                foreach (var data in staffdata)
-                {
-                    staff.ID = data.ID;
-                    staff.Username = data.Username;
-                    staff.FirstName = data.FirstName;
-                    staff.LastName = data.LastName;
-                }
-
-                return staff;
-            }
-            else
-            {
-                return new Staff()
-                {
-                    Username = "Wrong",
-                    FirstName = "fname",
-                    LastName = "lname"
-                };
-            }
-        }
 
         public static string[] Developers { get; } =
         {

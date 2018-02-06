485e4bdecd193ab377884e3bbe6431439d9b7a73
dddaria <daria.lewandowska24@gmail.com>
2018-02-06 13:14:01 +0100

    added seed for projectclass

--- ikt/ikt/Models/Project.cs
+++ ikt/ikt/Models/Project.cs
@@ -11,8 +11,6 @@ namespace ikt.Models
         public string Name { get; set; }
         public int? SubjectID { get; set; }
         public virtual Subject Subject { get; set; }
-        public int? ProjectClassID { get; set; }
-        public virtual ProjectClass Class { get; set; }
         public int Grade { get; set; }
         public string Description { get; set; }
         public string Date { get; set; }

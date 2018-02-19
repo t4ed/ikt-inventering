93978abfb5e3d2adb28b3764eb2cbdde55325ee4
dddaria <daria.lewandowska24@gmail.com>
2018-02-19 13:32:35 +0100

    search almost works

--- ikt/ikt/Controllers/HomeController.cs
+++ ikt/ikt/Controllers/HomeController.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ikt.Models;
+using ikt.ViewModels;
+using ikt.DAL;
+using System.Data.Entity;
 
 namespace ikt.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private iktContext db = new iktContext();
+        public ActionResult Index(string search)
         {
-            return View();
-        }
+            SearchIndexViewModel viewModel = new SearchIndexViewModel();
+            var projects = db.Projects.Include(p => p.Subject);
+            var ikts = db.Ikts.Include(i => i.Name);
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            if (!string.IsNullOrEmpty(search))
+            {
+                projects = projects.Where(p => p.Name.Contains(search) ||
+                p.Subject.Name.Contains(search));
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                ikts = ikts.Where(i => i.Name.Contains(search) ||
+                i.CreatedBy.Contains(search) ||
+                i.UpdatedBy.Contains(search));
+            }
 
             return View();
         }
---
+++ ikt/ikt/ViewModels/SearchIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ikt.Models;
+using PagedList;
+
+namespace ikt.ViewModels
+{
+    public class SearchIndexViewModel
+    {
+        public IPagedList<Project> Projects { get; set; }
+        public IPagedList<Ikt> IKTs { get; set; }
+        public string Search { get; set; }
+    }
+}

d4a0b2226a8739eb4145ec249e59540b6fdd78ae
Max Skogward <9826skma@jonkoping.se>
2018-02-22 13:00:04 +0100

    Bugfix s -> S

--- ikt/ikt/App_Start/BundleConfig.cs
+++ ikt/ikt/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace ikt
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }

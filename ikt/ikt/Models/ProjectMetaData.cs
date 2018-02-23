65fa4029bfb5b70bd3e8107020f79cf542da3e3d
johu96 <joel_ducati@hotmail.com>
2018-02-23 12:18:49 +0100

    completed navbar, started the create site

--- ikt/ikt/Models/ProjectMetaData.cs
+++ ikt/ikt/Models/ProjectMetaData.cs
@@ -6,14 +6,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ikt.Models
 {
+    [MetadataType(typeof(ProjectMetaData))]
     public partial class Project { }
-    public partial class ProjectMetaData
+    public class ProjectMetaData
     {
         [Required(ErrorMessage = "Namnet på projektet kan inte vara tomt.")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Namnet måste vara mellan 3 och 50 tecken långt.")]
         [RegularExpression(@"^[a-zåäöA-ZÅÄÖ0-9]+[a-zåäöA-ZÅÄÖ0-9'-'---\s]*$", ErrorMessage = "Namnet får inte innehålla några specialtecken.")]
         [Display(Name = "Projektnamn")]
         public string Name;
+
+        [Display(Name = "Ämne")]
+        public int? SubjectID;
 
         [Required(ErrorMessage = "Årskursen kan inte vara tom.")]
         [Range(1, 4, ErrorMessage = "Årskursen måste vara mellan 1-4")]

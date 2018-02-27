32fe00ed2c23285669b0b5b43b01ff818ae0f4f9
johu96 <joel_ducati@hotmail.com>
2018-02-27 13:54:50 +0100

    Fixed cross on date hover.

--- ikt/ikt/Scripts/JavaScript.js
+++ ikt/ikt/Scripts/JavaScript.js
@@ -17,9 +17,29 @@
         yearSuffix: '',
         showWeek: true
     });
-$("#SubjectID").autocomplete({
-    
-});
+
+    $(document).ready(function () {  
+        $("#SubjectID").autocomplete({  
+           source: function(request,response) {  
+               $.ajax({  
+                   url: "/FetchData/FetchSubjectData",  
+                   type: "GET",  
+                   dataType: "json",  
+                   data: { Prefix: request.term },  
+                   success: function (data) {  
+                       response($.map(data, function (item) {  
+                           return { label: item.Name, value: item.Name};  
+                       }))  
+  
+                   }  
+               })  
+           },  
+           messages: {  
+               noResults: "", results: ""  
+           }  
+       });  
+   })  
+
 $("#ClassID").autocomplete({
     
 });

78300b14e22b76e8d45b9888c0c7e10916fae19a
johu96 <joel_ducati@hotmail.com>
2018-02-27 11:05:09 +0100

    Added Jquery libr, fixed connections.

--- ikt/ikt/App_Start/BundleConfig.cs
+++ ikt/ikt/App_Start/BundleConfig.cs
@@ -14,6 +14,11 @@ namespace ikt
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/JavaScript.js",
+                        "~/Scripts/jquery-ui.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,7 +30,8 @@ namespace ikt
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/jquery-ui.css"));
         }
     }
 }

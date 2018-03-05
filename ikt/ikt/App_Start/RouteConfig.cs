ad892e3e5c0728b8e5728cb68357250f1a90763f
Marcus Perhamn <marcus@perhamn.se>
2018-03-05 12:58:40 +0100

    URLs fixed, and Ikts renamed to Tools in adress bar

--- ikt/ikt/App_Start/RouteConfig.cs
+++ ikt/ikt/App_Start/RouteConfig.cs
@@ -14,6 +14,42 @@ namespace ikt
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ToolsCreate",
+                url: "Tools/Create",
+                defaults: new { controller = "Ikts", action = "Create" }
+            );
+
+            routes.MapRoute(
+                name: "ToolsEdit",
+                url: "Tools/Edit/{id}",
+                defaults: new { controller = "Ikts", action = "Edit", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ToolsDetails",
+                url: "Tools/{id}",
+                defaults: new { controller = "Ikts", action = "Details", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ProjectsCreate",
+                url: "Projects/Create",
+                defaults: new { controller = "Projects", action = "Create" }
+            );
+
+            routes.MapRoute(
+                name: "ProjectsEdit",
+                url: "Projects/Edit/{id}",
+                defaults: new { controller = "Projects", action = "Edit", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ProjectDetails",
+                url: "Projects/{id}",
+                defaults: new { controller = "Projects", action = "Details", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
